6bcb5efb4ed9bc2bfe414f4e041d298f98ec9d69
Steve Westenbroek <smwesten@usgs.gov>
2018-03-12 15:50:54 -0500

    Update MacOS cmake script.

--- build/mac_brew/gcc/run_cmake_gcc.sh
+++ build/mac_brew/gcc/run_cmake_gcc.sh
@@ -10,13 +10,13 @@ rm -f *.txt
 
 # set important environment variables
 
-export GCC_VERSION=7.2.0
+export GCC_VERSION=6.4.0
 # set CMAKE-related and build-related variables
-export GCCLIST=$( glocate gcc-7 | grep Cellar | grep bin | grep $GCC_VERSION )
+export GCCLIST=$( glocate gcc-6 | grep Cellar | grep bin | grep $GCC_VERSION | grep -v 386 )
 export GCCARR=($GCCLIST)
 export CC=${GCCARR[1]}
 
-export GXXLIST=$( glocate g++-7 | grep Cellar | grep bin | grep $GCC_VERSION )
+export GXXLIST=$( glocate g++-6 | grep Cellar | grep bin | grep $GCC_VERSION | grep -v 386 )
 export GXXARR=($GXXLIST)
 export CXX=${GXXARR[1]}
 
@@ -24,7 +24,7 @@ export CMAKEROOT=/usr/bin/cmake
 export R_HOME=/usr/bin/R
 
 
-export LIB_GDAL=$(glocate gdal.dylib | grep Cellar )
+export LIB_GDAL=$(glocate libgdal.dylib | grep Cellar )
 export CMAKE_EXE_LINKER_FLAGS=$(gdal-config --dep-libs)
 #export LIB_GCC=$( glocate libgcc.a | grep Cellar | grep $GCC_VERSION | grep -v i386 )
 #export LIB_GXX=$( glocate libstdc++.a | grep Cellar | grep $GCC_VERSION | grep -v i386 | grep -v gdb.py )
@@ -40,8 +40,8 @@ export BUILD_TYPE="DEBUG"
 export SYSTEM_TYPE="MacOS"
 
 # define platform and compiler specific compilation flags
-export CMAKE_CXX_FLAGS_DEBUG="-O0 -g -ggdb"
-export CMAKE_CXX_FLAGS_RELEASE="-O2 -mtune=native -Wno-maybe-uninitialized"
+export CMAKE_CXX_FLAGS_DEBUG="-O0 -g -ggdb $(gdal-config --cflags)"
+export CMAKE_CXX_FLAGS_RELEASE="-O2 -mtune=native -Wno-maybe-uninitialized $(gdal-config --cflags)"
 
 cmake "../../.." -G "Unix Makefiles"                         \
 -DSYSTEM_TYPE="$SYSTEM_TYPE "                                \
